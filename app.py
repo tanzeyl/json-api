0895aedd6c018309cf9f241653bf2cd7892bca06
Tanzeel <ktanzeel80@gmail.com>
2022-07-20 18:05:23 +0530

    Made changes in the return type

--- app.py
+++ app.py
@@ -1,37 +1,28 @@
 from flask import Flask
-import re
 import json
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['POST'])
 def home():
-  images = []
-  names = []
-  discounts = []
-  prices = []
-  prices1 = []
-  categories = []
-  stocks = []
-  main = []
+  main = {}
+  main["images"] = []
+  main["names"] = []
+  main["discounts"] = []
+  main["prices"] = []
+  main["prices1"] = []
+  main["categories"] = []
+  main["stocks"] = []
   f = open("db.json")
   data = json.load(f)
   for property in data["Products"]:
-    images.append(property["image_url"])
-    names.append(property["name"])
-    discounts.append(property["discount"])
-    prices.append(property["price"])
-    prices1.append(property["price1"])
-    categories.append(property["category"])
-    stocks.append(property["Stock"])
-    main.append(images)
-    main.append(names)
-    main.append(discounts)
-    main.append(prices)
-    main.append(prices1)
-    main.append(categories)
-    main.append(stocks)
+    main["images"].append(property["image_url"])
+    main["names"].append(property["name"])
+    main["discounts"].append(property["discount"])
+    main["prices"].append(property["price"])
+    main["prices1"].append(property["price1"])
+    main["categories"].append(property["category"])
+    main["stocks"].append(property["Stock"])
   return main
 
 if __name__ == "__main__":
